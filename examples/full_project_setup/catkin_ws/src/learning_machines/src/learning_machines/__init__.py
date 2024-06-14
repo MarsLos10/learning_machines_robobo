00cfec6e487083eb839048d6db495bfda057252b
xjjjjjjj <baruchxm@gmail.com>
2024-06-14 23:52:47 +0200

    restore

--- examples/full_project_setup/catkin_ws/src/learning_machines/src/learning_machines/__init__.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/src/learning_machines/__init__.py
@@ -1,5 +1,3 @@
-from .test_actions import HardwareRobobo, task1
+from .test_actions import run_all_actions
 
-
-__all__ = ("HardwareRobobo", task1)
-# __all__ = ("run_all_actions", "HardwareRobobo", "move_till_obstacle", "plot_sensor", "plot_avg_sensor")
+__all__ = ("run_all_actions",)
